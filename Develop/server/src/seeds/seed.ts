b22cdf8583ee5dd61974a7354e1ef869f3ec8d90
Andrew <pelfreya6@gmail.com>
2025-03-25 12:14:02 -0400

    render

--- Develop/server/src/seeds/seed.ts
+++ Develop/server/src/seeds/seed.ts
@@ -2,7 +2,8 @@ import db from "../config/connection.js";
 import Question from "../models/Question.js";
 import cleanDB from "./cleanDb.js";
 
-import pythonQuestions from './pythonQuestions.json' assert { type: 'json' };
+import fs from 'fs';
+const pythonQuestions = JSON.parse(fs.readFileSync('./pythonQuestions.json', 'utf8'));
 
 db.once('open', async () => {
   await cleanDB('Question', 'questions');
