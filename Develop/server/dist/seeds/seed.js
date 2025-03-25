d6e2f8084f73d88edb710e02033d630f67fa048a
Andrew <pelfreya6@gmail.com>
2025-03-25 12:26:48 -0400

    renderfix

--- Develop/server/dist/seeds/seed.js
+++ Develop/server/dist/seeds/seed.js
@@ -1,7 +1,12 @@
 import db from "../config/connection.js";
 import Question from "../models/Question.js";
 import cleanDB from "./cleanDb.js";
-import pythonQuestions from './pythonQuestions.json' assert { type: "json" };
+import fs from 'fs';
+import path from 'path';
+
+const pythonQuestionsPath = path.join(__dirname, 'pythonQuestions.json');
+const pythonQuestions = JSON.parse(fs.readFileSync(pythonQuestionsPath, 'utf-8'));
+console.log(pythonQuestions);
 db.once('open', async () => {
     await cleanDB('Question', 'questions');
     await Question.insertMany(pythonQuestions);
