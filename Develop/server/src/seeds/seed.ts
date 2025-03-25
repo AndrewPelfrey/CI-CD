85164915e3fa53fbb790e340831c4c690c70dd1f
Andrew <pelfreya6@gmail.com>
2025-03-25 12:32:32 -0400

    render

--- Develop/server/src/seeds/seed.ts
+++ Develop/server/src/seeds/seed.ts
@@ -4,6 +4,9 @@ import cleanDB from "./cleanDb.js";
 import fs from 'fs';
 import path from 'path';
 
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+
 const pythonQuestionsPath = path.join(__dirname, 'pythonQuestions.json');
 const pythonQuestions = JSON.parse(fs.readFileSync(pythonQuestionsPath, 'utf-8'));
 
